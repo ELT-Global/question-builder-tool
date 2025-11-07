004f07be07a51992a0aa0f362243636cfd7bb346
arshadakl <66788460+arshadakl@users.noreply.github.com>
2025-11-07 18:03:47 +0530

    feat: deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // 👈 generates static files
-  images: { unoptimized: true }, // optional: disables Next.js Image Optimization
-  basePath: '/question-builder-tool', // 👈 replace with your repo name
-  assetPrefix: '/question-builder-tool/', // 👈 same here
-};
-
-module.exports = nextConfig;
-
-
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-//   images: {
-//     unoptimized: true,
-//   },
-// }
+//   output: 'export', // 👈 generates static files
+//   images: { unoptimized: true }, // optional: disables Next.js Image Optimization
+//   basePath: '/question-builder-tool', // 👈 replace with your repo name
+//   assetPrefix: '/question-builder-tool/', // 👈 same here
+// };
 
-// export default nextConfig
+// module.exports = nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
